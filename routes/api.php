9f93a6082fb9469edca25bdcbad98c74bf169576
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-09-06 15:17:46 +0700

    remove controller auth in api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::post("register",[AuthController::class,"register"]);
-Route::post("login",[AuthController::class,"login"]);
 
 Route::middleware('auth.sectoken')->group(function(){
     Route::get("order",[TransactionController::class,"order"]);
